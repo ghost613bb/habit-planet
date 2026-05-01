ce279992d23114d708019fd841587531eee96121
wangjingting.613 <wangjingting.613@bytedance.com>
2026-05-01 14:59:57 +0800

    feat: 调整帐篷周围树木摆放位置

--- src/utils/planet/layers/VegetationLayer.ts
+++ src/utils/planet/layers/VegetationLayer.ts
@@ -303,8 +303,11 @@ export class VegetationLayer implements LayerController {
 
   private createTrees() {
     const anchors = [
+      // 帐篷左侧直杆树（勿删）
       { phi: 0.48, theta: 5.72 },
-      { phi: 0.2, theta: 2.7 },
+      // 帐篷右侧直杆树（勿删）
+      { phi: 0.3, theta: 3.7 },
+      // 帐篷右侧多枝干树（勿删）
       { phi: 0.48, theta: 2.62 },
       { phi: 0.24, theta: 4.8 },
     ]
